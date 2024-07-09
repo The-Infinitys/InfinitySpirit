e6d38148b8aca4aab2f4210d4a1458d8af76925c
The Infinity's <137460810+The-Infinitys@users.noreply.github.com>
2024-07-09 12:55:20 +0900

    Update convert.py

--- InfinitySpirit/script/convert.py
+++ InfinitySpirit/script/convert.py
@@ -113,7 +113,7 @@ def convert(markdown_path) -> int:
     with open(markdown_path[:-3] + ".html", mode="w") as index_html:
         index_html.write(export_html)
     with open(markdown_path[:-3] + ".css", mode="w") as style_css:
-        style_css.write(open("./InfinitySpirit/template/style.css").read())
+        style_css.write(open("./InfinitySpirit/template/"+loadsetting.load()["template"]+"/style.css").read())
     with open(markdown_path[:-3] + ".js", mode="w") as script_js:
-        script_js.write(open("./InfinitySpirit/template/style.css").read())
+        script_js.write(open("./InfinitySpirit/template/"+loadsetting.load()["template"]+"/script.js")read())
     return 0
